7f0eb8d2c81c272fc72b0702242ae936f87ee998
Rajiv <kumarrajiv0920@gmail.com>
2025-09-29 00:24:39 +0530

    Fix: Pricing Responsiveness

--- src/components/PricingSection/PricingCard.jsx
+++ src/components/PricingSection/PricingCard.jsx
@@ -12,21 +12,17 @@ const PricingCard = ({
   message,
 }) => {
   return (
-    <div className="pricing-card p-5 md:min-w-1/5 xl:min-w-1/3 flex flex-col min-w-[90%] max-w-[90%] mx-auto">
-      {/* <div className={`top-${blur}-blur`}></div> */}
+    <div className="pricing-card p-5 flex flex-col min-w-[90%] max-w-[90%] mx-auto">
       <div className={`top-left-blur`}></div>
       <div className={`bottom-right-blur`}></div>
-      {/* <div
-        className={`bottom-${blur === "left" ? "right" : "left"}-blur`}
-      ></div> */}
       <div className="pricing-info mb-7">
         <div className="type">{name}</div>
-        <div className="discount text-sm sm:text-md">
+        <div className="discount text-sm md:text-md">
           <span className="offer">Save {savePercentage}%</span>{" "}
           <span>₹{originalPrice}</span>
         </div>
         <div className="price mt-5 text-sm sm:text-md">
-          <span className="text-4xl sm:text-5xl font-bold">
+          <span className="text-3xl sm:text-4xl lg:text-5xl font-bold">
             ₹{discountedPrice}
           </span>
           /project
@@ -39,8 +35,8 @@ const PricingCard = ({
               key={index}
               className="text-sm flex list-none gap-3 text-white items-center"
             >
-              <BsPatchCheckFill fill="white" />
-              {f}
+              <BsPatchCheckFill fill="white" className="w-[20px]" />
+              <span className="flex-1">{f}</span>
             </li>
           ))}
         </ul>
@@ -50,7 +46,7 @@ const PricingCard = ({
           href={`https://wa.me/+917364877930?text=${encodeURIComponent(
             message
           )}`}
-          className="btn primary text-center mt-10"
+          className="btn primary text-center mt-10 md:text-sm"
         >
           {ctaText}
         </a>
