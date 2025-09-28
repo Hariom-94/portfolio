9437656de578a33889a31fc583ce103d0a265fa4
Rajiv <kumarrajiv0920@gmail.com>
2025-09-28 20:28:29 +0530

    Poster Image Added

--- src/components/ProjectsSection/VideoGrid.jsx
+++ src/components/ProjectsSection/VideoGrid.jsx
@@ -6,14 +6,14 @@ const videos = [
   {
     id: 1,
     poster:
-      "https://raw.githubusercontent.com/Hariom-94/portfolio/main/src/assets/thumbnail%20nepalGenZ.png",
+      "https://raw.githubusercontent.com/Hariom-94/portfolio/main/src/assets/hrvgu4dowjqzptf2rq6w.webp",
     videoEmbed:
       "https://player.mux.com/m3L2HIK4SxNdl00q1eggbzVnz4CaOkOaTo5y3EQ76ju8?metadata-video-title=genZ+Nepal&video-title=genZ+Nepal",
   },
   {
     id: 2,
     poster:
-      "https://raw.githubusercontent.com/Hariom-94/portfolio/main/src/assets/thumbnail%20amazon.png",
+      "https://raw.githubusercontent.com/Hariom-94/portfolio/main/src/assets/zaqi6igt9nqf4gm8gkp2.webp",
     videoEmbed:
       "https://player.mux.com/TbBrc2IYoJ54Vla3ePlLqPw2EKfifUqT37n01apxnkXQ?metadata-video-title=TRUTH+BEHIND+AMAZON+LOGO&video-title=TRUTH+BEHIND+AMAZON+LOGO",
   },
