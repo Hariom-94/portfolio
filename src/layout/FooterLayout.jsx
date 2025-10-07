c892dbbf5447075e1b8d82408bcd9750c8a31677
Rajiv <kumarrajiv0920@gmail.com>
2025-10-08 01:50:42 +0530

    Fiverr Link Added

--- src/layout/FooterLayout.jsx
+++ src/layout/FooterLayout.jsx
@@ -1,13 +1,20 @@
+import { TbBrandFiverr } from "react-icons/tb";
 import Logo from "../assets/logo.png";
+import {
+  FaEnvelope,
+  FaInstagram,
+  FaLinkedinIn,
+  FaWhatsapp,
+} from "react-icons/fa";
 
 const FooterLayout = () => {
   return (
-    <footer className="flex flex-col gap-7 text-center items-center py-5 sm:flex-row sm:justify-between sm:items-center sm:px-20 md:px-30">
+    <footer className="flex flex-col gap-6 text-center items-center py-5 md:flex-row sm:justify-between sm:items-center sm:px-20 md:px-30">
       <a href="#home" className="logo">
         <img src={Logo} alt="" className="max-w-[90px] max-h-[90px]" />
       </a>
       <div className="navigation">
-        <ul className="flex flex-col gap-2 sm:flex-row sm:justify-between sm:gap-8">
+        <ul className="flex flex-col gap-2 md:flex-row sm:justify-between sm:gap-8">
           <li>
             <a href="#home">Home</a>
           </li>
@@ -22,22 +29,25 @@ const FooterLayout = () => {
           </li>
         </ul>
       </div>
-      <div className="links flex gap-3 justify-center text-xl">
+      <div className="links flex gap-4 justify-center text-xl">
         <a
           href={`https://wa.me/+917364877930?text=${encodeURIComponent(
             "Hi! I was just looking through your website and wanted to connect."
           )}`}
         >
-          <i class="fa-brands fa-whatsapp"></i>
+          <FaWhatsapp />
         </a>
         <a href="https://www.instagram.com/harii_0m_">
-          <i class="fa-brands fa-instagram"></i>
+          <FaInstagram />
         </a>
         <a href="mailto:hariomkumar8988@gmail.com">
-          <i class="fa-solid fa-envelope"></i>
+          <FaEnvelope />
         </a>
         <a href="https://www.linkedin.com/in/hariiomkumar">
-          <i class="fa-brands fa-linkedin-in"></i>{" "}
+          <FaLinkedinIn />
+        </a>
+        <a href="https://www.fiverr.com/hariom_89">
+          <TbBrandFiverr />
         </a>
       </div>
     </footer>
